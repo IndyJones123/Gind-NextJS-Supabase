7e9fb2405ee648ca490e9465ff9e8b0482f39a06
Alfian Dorif Murtadlo <alfianitem999@gmail.com>
2024-05-01 11:21:34 +0700

    Update

--- components/global/WrapperGame.tsx
+++ components/global/WrapperGame.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { ReactElement } from "react";
 
 type ParamsProps = {
@@ -20,7 +21,7 @@ const WrapperGame = ({
       <div className="w-full sm:w-96">
         <div className="card bg-base-300 shadow-xl h-full">
           <figure className="h-48 sm:h-auto">
-            <img
+            <Image
               src={image}
               alt="Shoes"
               className="object-cover w-full h-full"
