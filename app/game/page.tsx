74c8c8af6e305d51b88e3b7ecaa581ee6e548a7e
Alfian Dorif Murtadlo <alfianitem999@gmail.com>
2024-05-01 11:11:53 +0700

    Update

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -11,27 +11,29 @@ const Page = async (): Promise<React.ReactElement> => {
 
   return (
     <Wrapper title="List Game">
-      <div className="skeleton w-full h-2 stats shadow container mx-auto mb-8 "></div>
-      <div className="flex xl:flex-row gap-5">
-        {data.data &&
-          data.data.map(
-            (
-              game // Change variable name from "games" to "game"
-            ) => (
-              <WrapperGame
-                key={game.id} // Add a unique key prop using the game's ID or another unique identifier
-                title={game.namagame}
-                description={game.description}
-                image={game.gambar}
-                genre={game.genre}>
-                <Link // Nest children between the opening and closing tags of WrapperGame
-                  href={game.LinkDownload ? game.LinkDownload : "/error"}
-                  className="btn btn-info">
-                  Download Game
-                </Link>
-              </WrapperGame>
-            )
-          )}
+      <div>
+        <div className="skeleton w-full h-2 stats shadow container mx-auto mb-8 "></div>
+        <div className="flex xl:flex-row gap-5">
+          {data.data &&
+            data.data.map(
+              (
+                game // Change variable name from "games" to "game"
+              ) => (
+                <WrapperGame
+                  key={game.id} // Add a unique key prop using the game's ID or another unique identifier
+                  title={game.namagame}
+                  description={game.description}
+                  image={game.gambar}
+                  genre={game.genre}>
+                  <Link // Nest children between the opening and closing tags of WrapperGame
+                    href={game.LinkDownload ? game.LinkDownload : "/error"}
+                    className="btn btn-info">
+                    Download Game
+                  </Link>
+                </WrapperGame>
+              )
+            )}
+        </div>
       </div>
     </Wrapper>
   );
